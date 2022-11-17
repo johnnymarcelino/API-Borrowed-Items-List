ddced6bc9dd10606111a896a801b766284ce3a57
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-11-17 10:56:08 -0300

    @johnnymarcelino [API of borrowed items list started] {nov.17.2022 10:55am}

---
+++ API-Borrowed-Items-List/API-Borrowed-Items-List/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API_Borrowed_Items_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
